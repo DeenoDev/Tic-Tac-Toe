eddf5b1d4ffa4563ca666032db5dfceaf084a340
DeenoDev <deyanoklaasen@outlook.com>
2022-10-20 23:18:11 +0200

    created winning conditions array.

--- main.js
+++ main.js
@@ -14,16 +14,15 @@
 
 
 
-const winningConditions = {
+const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
     [0, 4, 8],
-    [2, 4, 6]
-
-}
+    [2, 4, 6],
+];
 
 const form = document.querySelector("#myForm");
 
